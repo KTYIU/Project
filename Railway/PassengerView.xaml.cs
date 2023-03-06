ced3d48bc2462d25200c3f4fb3a640b8981b776a
kathyT <124563688+KTYIU@users.noreply.github.com>
2023-03-06 11:38:11 +0200

    Kt updates (#7)
    * design changes
    
    * Design changes 2
    
    * design updates 2
    
    * Delete .vs/Railway/v16 directory
    
    * PassengerView window added
    
    * Delete Railway/obj directory
    
    ---------
    
    Co-authored-by: k.toncheva23 <k.toncheva23@LABSCIFIPC16>

---
+++ Railway/PassengerView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Railway
+{
+    /// <summary>
+    /// Interaction logic for PassengerView.xaml
+    /// </summary>
+    public partial class PassengerView : Window
+    {
+        public PassengerView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void accountActionsBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
